bb917e42f422e3f9302811c0919a04aabd5824c5
wmeissner <wmeissner@3c146ecf-7c30-0410-aadf-9b72ef142954>
2007-05-15 08:06:59 +0000

    Add unit test

---
+++ test/org/gstreamer/test/InitTest.java
@@ -0,0 +1,45 @@
+/*
+ * InitTest.java
+ * JUnit 4.x based test
+ */
+
+package org.gstreamer.test;
+
+import org.gstreamer.Gst;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class InitTest {
+    
+    public InitTest() {
+        
+    }
+    @Test
+    public void testInit() {
+        String[] args = Gst.init("foo", new String[] { "--gst-plugin-spew" });
+        //assertTrue(args.length == 0);
+    }
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
