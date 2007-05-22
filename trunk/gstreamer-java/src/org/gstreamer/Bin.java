4e2475fe0dda20a2b2eb1063c2e88a3f1aa42b76
wmeissner <wmeissner@3c146ecf-7c30-0410-aadf-9b72ef142954>
2007-05-22 01:41:33 +0000

    Make license LGPL, and splat licence header at the top of each file.

--- trunk/gstreamer-java/src/org/gstreamer/Bin.java
+++ trunk/gstreamer-java/src/org/gstreamer/Bin.java
@@ -1,5 +1,13 @@
-/*
- * Bin.java
+/* 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  */
 
 package org.gstreamer;
@@ -9,7 +17,6 @@ import org.gstreamer.lowlevel.GstAPI;
 import com.sun.jna.Pointer;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import org.gstreamer.event.BinListener;
--- trunk/gstreamer-java/test/org/gstreamer/BinTest.java
+++ trunk/gstreamer-java/test/org/gstreamer/BinTest.java
@@ -1,8 +1,13 @@
-/*
- * BinTest.java
- * JUnit 4.x based test
+/* 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * Created on 18 May 2007, 14:03
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  */
 
 package org.gstreamer;
