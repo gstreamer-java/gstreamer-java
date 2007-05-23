2296f8280de02665fcfaa8cc8d2b2255ef6a9a38
wmeissner <wmeissner@3c146ecf-7c30-0410-aadf-9b72ef142954>
2007-05-23 23:58:02 +0000

    Throw an exception if anyone tries to use the GMainLoop when using the GTK Look and Feel.
    See: http://code.google.com/p/gstreamer-java/issues/detail?id=6

--- gstreamer-java/src/org/gstreamer/GMainLoop.java
+++ gstreamer-java/src/org/gstreamer/GMainLoop.java
@@ -15,6 +15,7 @@ package org.gstreamer;
 import com.sun.jna.Pointer;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import javax.swing.UIManager;
 import org.gstreamer.lowlevel.GlibAPI;
 
 /**
@@ -22,22 +23,34 @@ import org.gstreamer.lowlevel.GlibAPI;
  */
 public class GMainLoop extends NativeObject implements Runnable {
     private static GlibAPI glib = GlibAPI.glib;
-
+    
     /** Creates a new instance of GMainLoop */
     public GMainLoop() {
         this(glib.g_main_loop_new(null, false), false, true);
     }
+    /*
+     * Due to a bug in the GTK bridge, you cannot use the Glib main loop with 
+     * the GTK look and feel, so throw an exception if someone tries.
+     */
+    private void checkLAF() {
+        if (UIManager.getLookAndFeel().getClass() == com.sun.java.swing.plaf.gtk.GTKLookAndFeel.class) {
+            throw new RuntimeException("Cannot use GTK look and feel with GMainLoop\n" +
+                    "\nSee http://code.google.com/p/gstreamer-java/issues/detail?id=6\n");
+        }
+    }
     GMainLoop(Pointer ptr, boolean needRef, boolean ownsHandle) {
         super(ptr, needRef, ownsHandle);
+        checkLAF();
     }
     public void quit() {
         Gst.invokeLater(new Runnable() {
             public void run() {
                 glib.g_main_loop_quit(handle());
             }
-        });   
+        });
     }
     public void run() {
+        checkLAF();
         glib.g_main_loop_run(handle());
     }
     
@@ -46,6 +59,7 @@ public class GMainLoop extends NativeObject implements Runnable {
     }
     
     public void startInBackground() {
+        checkLAF();
         bgThread = new java.lang.Thread(this);
         bgThread.setDaemon(true);
         bgThread.setName("gmainloop");
@@ -76,7 +90,7 @@ public class GMainLoop extends NativeObject implements Runnable {
     void unref() {
         glib.g_main_loop_unref(handle());
     }
-
+    
     void disposeNativeHandle(Pointer ptr) {
         glib.g_main_loop_unref(ptr);
     }
