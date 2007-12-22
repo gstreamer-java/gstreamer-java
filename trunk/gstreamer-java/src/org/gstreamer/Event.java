f2378a74c76ed64b14e306219f62261d58cfd48b
wmeissner <wmeissner@3c146ecf-7c30-0410-aadf-9b72ef142954>
2007-12-22 01:04:32 +0000

    Update copyright

--- trunk/gstreamer-java/src/org/gstreamer/Event.java
+++ trunk/gstreamer-java/src/org/gstreamer/Event.java
@@ -1,4 +1,6 @@
 /* 
+ * Copyright (c) 2007 Wayne Meissner
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
