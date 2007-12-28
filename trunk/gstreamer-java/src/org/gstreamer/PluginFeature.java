9c7bdc7223a46bcfa91c89e17327cf4049974c9d
wmeissner <wmeissner@3c146ecf-7c30-0410-aadf-9b72ef142954>
2007-12-28 12:08:41 +0000

    Update Plugin and PluginFeature javadoc

--- trunk/gstreamer-java/src/org/gstreamer/PluginFeature.java
+++ trunk/gstreamer-java/src/org/gstreamer/PluginFeature.java
@@ -1,5 +1,7 @@
 /* 
- * Copyright (c) 2007 Wayne Meissner
+ * Copyright (C) 2007 Wayne Meissner
+ * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
+ *                    2000 Wim Taymans <wtay@chello.be>
  * 
  * This file is part of gstreamer-java.
  *
@@ -22,6 +24,12 @@ package org.gstreamer;
 import java.util.logging.Logger;
 import static org.gstreamer.lowlevel.GstAPI.gst;
 
+/**
+ * Base class for contents of a {@link Plugin}
+ *
+ * This is a base class for anything that can be added to a Plugin.
+ * @see Plugin
+ */
 public class PluginFeature extends GstObject {
     private static Logger logger = Logger.getLogger(PluginFeature.class.getName());
     
@@ -29,15 +37,60 @@ public class PluginFeature extends GstObject {
     PluginFeature(Initializer init) { 
         super(init); 
     }
+    
+    @Override
     public String toString() {
         return getName();
     }
+    
+    /**
+     *  Gets the name of a plugin feature.
+     *
+     * @return The name.
+     */
+    @Override
     public String getName() {
         return gst.gst_plugin_feature_get_name(this);
     }
+    
+    /**
+     * Sets the name of a plugin feature. The name uniquely identifies a feature
+     * within all features of the same type. Renaming a plugin feature is not
+     * allowed.
+     * @param name The name to set.
+     */
+    @Override
+    public void setName(String name) {
+        gst.gst_plugin_feature_set_name(this, name);
+    }
+    
+    /**
+     * Set the rank for the plugin feature.
+     * Specifies a rank for a plugin feature, so that autoplugging uses
+     * the most appropriate feature.
+     * @param rank The rank value - higher number means more priority rank
+     */
+    public void setRank(int rank) {
+        gst.gst_plugin_feature_set_rank(this, rank);
+    }
+    
+    /**
+     * Gets the rank of a plugin feature.
+     *
+     * @return The rank of the feature.
+     */
     public int getRank() {
         return gst.gst_plugin_feature_get_rank(this);
     }
+    
+    /**
+     * Checks whether the given plugin feature is at least the required version.
+     *
+     * @param major Minimum required major version
+     * @param minor Minimum required minor version
+     * @param micro Minimum required micro version
+     * @return true if the plugin feature has at least the required version, otherwise false.
+     */
     public boolean checkVersion(int major, int minor, int micro) {
         return gst.gst_plugin_feature_check_version(this, minor, minor, micro);
     }
