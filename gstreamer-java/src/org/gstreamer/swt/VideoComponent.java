54e0a754bc9447bf8d25b3a95e94cdef6ad55d6d
lfarkas <lfarkas@3c146ecf-7c30-0410-aadf-9b72ef142954>
2009-02-19 09:10:34 +0000

    update and reindent SWT VideoComponent

--- gstreamer-java/src/org/gstreamer/swt/VideoComponent.java
+++ gstreamer-java/src/org/gstreamer/swt/VideoComponent.java
@@ -27,6 +27,7 @@ import java.awt.image.DataBufferInt;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
@@ -39,166 +40,179 @@ import org.gstreamer.elements.RGBDataSink;
 
 public class VideoComponent extends Canvas {
 
-    private BufferedImage currentImage = null;
-    private final RGBDataSink videosink;
-    private final Lock bufferLock = new ReentrantLock();
-    private boolean updatePending = false;
-    private int alpha = 255;
-    private String ovText;
-    private long start = System.currentTimeMillis();
-    private boolean keepAspect = true;
-    private boolean showOverlay = false;
-    private boolean showFPS = false;
+	private BufferedImage currentImage = null;
+	private final RGBDataSink videosink;
+	private final Lock bufferLock = new ReentrantLock();
+	private boolean updatePending = false;
+	private int alpha = 255;
+	private String ovText;
+	private long start = System.currentTimeMillis();
+	private boolean keepAspect = true;
+	private boolean showOverlay = false;
+	private boolean showFPS = false;
+	private Color bgColor;
 
-    public VideoComponent(final Composite parent, int style) {
-        super(parent, style);
+	public VideoComponent(final Composite parent, int style) {
+		super(parent, style);
 
-        videosink = new RGBDataSink("GstVideoComponent", new RGBListener());
-        videosink.setPassDirectBuffer(true);
+		videosink = new RGBDataSink("GstVideoComponent", new RGBListener());
+		videosink.setPassDirectBuffer(true);
 
-        final Font font = new Font(getDisplay(), "Arial", 13, SWT.NORMAL);
+		final Font font = new Font(getDisplay(), "Arial", 13, SWT.NORMAL);
 
-        this.addPaintListener(new PaintListener() {
+		this.addPaintListener(new PaintListener() {
 
-            public void paintControl(PaintEvent event) {
-                Point cSize = getSize();
-                if (currentImage != null) {
-                    event.gc.setFont(font);
-                    int newX = 0, newY = 0;
-                    int fps = 0;
+			public void paintControl(PaintEvent event) {
+				Point cSize = getSize();
+				if (currentImage != null) {
+					event.gc.setFont(font);
+					int newX = 0, newY = 0;
+					int fps = 0;
 
-                    int[] Frame = ((DataBufferInt) currentImage.getRaster().getDataBuffer()).getData();
-                    ImageData imgdata = new ImageData(currentImage.getWidth(),
-                            currentImage.getHeight(), 24, new PaletteData(
-                            0xFF0000, 0x00FF00, 0x0000FF));
-                    imgdata.setPixels(0, 0, currentImage.getWidth() * currentImage.getHeight(), Frame, 0);
+					int[] Frame = ((DataBufferInt) currentImage.getRaster().getDataBuffer()).getData();
+					ImageData imgdata
+						= new ImageData(currentImage.getWidth(), currentImage.getHeight(), 24,
+										new PaletteData(0xFF0000, 0x00FF00, 0x0000FF));
+					imgdata.setPixels(0, 0, currentImage.getWidth() * currentImage.getHeight(), Frame, 0);
 
-                    if ((currentImage.getWidth() != cSize.x) || (currentImage.getHeight() != cSize.y)) {
-                        int sizeX = cSize.x, sizeY = cSize.y;
-                        event.gc.setInterpolation(SWT.HIGH);
-                        if (keepAspect) {
-                            if (((float) currentImage.getWidth() / (float) cSize.x) > ((float) currentImage.getHeight() / (float) cSize.y)) {
-                                sizeY = cSize.x * currentImage.getHeight() / currentImage.getWidth();
-                                newY = (cSize.y - sizeY) / 2;
-                            } else {
-                                sizeX = cSize.y * currentImage.getWidth() / currentImage.getHeight();
-                                newX = (cSize.x - sizeX) / 2;
-                            }
-                        }
-                        imgdata = imgdata.scaledTo(sizeX, sizeY);
-                    }
+					if ((currentImage.getWidth() != cSize.x) || (currentImage.getHeight() != cSize.y)) {
+						int sizeX = cSize.x, sizeY = cSize.y;
+						event.gc.setInterpolation(SWT.HIGH);
+						if (keepAspect) {
+							if (((float) currentImage.getWidth() / (float) cSize.x)
+								> ((float) currentImage.getHeight() / (float) cSize.y)) {
+								sizeY = cSize.x * currentImage.getHeight()
+										/ currentImage.getWidth();
+								newY = (cSize.y - sizeY) / 2;
+							} else {
+								sizeX = cSize.y * currentImage.getWidth()
+										/ currentImage.getHeight();
+								newX = (cSize.x - sizeX) / 2;
+							}
+						}
+						imgdata = imgdata.scaledTo(sizeX, sizeY);
+					}
 
-                    if (alpha != event.gc.getAlpha()) {
-                        event.gc.setAlpha(alpha);
-                    }
-                    Image image = new Image(parent.getDisplay(), imgdata);
-                    event.gc.drawImage(image, newX, newY);
+					if (alpha != event.gc.getAlpha()) {
+						event.gc.setAlpha(alpha);
+					}
+					Image image = new Image(parent.getDisplay(), imgdata);
+					event.gc.drawImage(image, newX, newY);
 
-                    if (showFPS) {
-                        fps = (int) (1000 / (System.currentTimeMillis() - start));
-                    }
+					if (showFPS) {
+						fps = (int) (1000 / (System.currentTimeMillis() - start));
+					}
 
-                    if (showOverlay) {
-                        event.gc.drawText(ovText, newX + 5, newY + 5, false);
-                        newY += 20;
-                    }
-                    if (showFPS) {
-                        event.gc.drawText(" FPS:" + fps, newX + 5, newY + 5,
-                                false);
-                    }
-                    image.dispose();
-                    if (showFPS) {
-                        start = System.currentTimeMillis();
-                    }
-                } else {
-                    Image image = new Image(parent.getDisplay(), cSize.x,
-                            cSize.y);
-                    event.gc.drawImage(image, 0, 0);
-                    event.gc.drawText("SWT canvas created", 10, 10);
-                    image.dispose();
-                }
-            }
-        });
-    }
+					if (showOverlay) {
+						event.gc.drawText(ovText, newX + 5, newY + 5, false);
+						newY += 20;
+					}
+					if (showFPS) {
+						event.gc.drawText(" FPS:" + fps, newX + 5, newY + 5,
+								false);
+					}
+					image.dispose();
+					if (showFPS) {
+						start = System.currentTimeMillis();
+					}
+				} else {
+					if (bgColor != null) {
+						event.gc.setBackground(bgColor);
+						event.gc.fillRectangle(0, 0, cSize.x, cSize.y);
+					}
+				}
+			}
+		});
+	}
 
-    public Element getElement() {
-        return videosink;
-    }
+	public Element getElement() {
+		return videosink;
+	}
 
-    public void setKeepAspect(boolean keepAspect) {
-        this.keepAspect = keepAspect;
-    }
+	public void setKeepAspect(boolean keepAspect) {
+		this.keepAspect = keepAspect;
+	}
 
-    public void setAlpha(int alpha) {
-        this.alpha = alpha;
-    }
+	public void setAlpha(int alpha) {
+		this.alpha = alpha;
+	}
 
-    public void setOverlay(String text) {
-        this.ovText = text;
-    }
+	public void setOverlay(String text) {
+		this.ovText = text;
+	}
 
-    public void showFPS(boolean bn) {
-        this.showFPS = bn;
-    }
+	public void showFPS(boolean bn) {
+		this.showFPS = bn;
+	}
 
-    public void showOverlay(boolean bn) {
-        this.showOverlay = bn;
-    }
+	public void showOverlay(boolean bn) {
+		this.showOverlay = bn;
+	}
 
-    public int getAlpha() {
-        return alpha;
-    }
-    private final Runnable update = new Runnable() {
+	public int getAlpha() {
+		return alpha;
+	}
 
-        public void run() {
-            bufferLock.lock();
-            try {
-                if (!isDisposed()) {
-                    redraw();
-                }
-                updatePending = false;
-            } finally {
-                bufferLock.unlock();
-            }
-        }
-    };
+	private final Runnable update = new Runnable() {
 
-    private BufferedImage getBufferedImage(int width, int height) {
-        if (currentImage != null && currentImage.getWidth() == width && currentImage.getHeight() == height) {
-            return currentImage;
-        }
-        if (currentImage != null) {
-            currentImage.flush();
-        }
-        currentImage = new BufferedImage(width, height,
-                BufferedImage.TYPE_INT_RGB);
-        currentImage.setAccelerationPriority(0.0f);
-        return currentImage;
-    }
+		public void run() {
+			bufferLock.lock();
+			try {
+				if (!isDisposed()) {
+					redraw();
+				}
+				updatePending = false;
+			} finally {
+				bufferLock.unlock();
+			}
+		}
+	};
 
-    private class RGBListener implements RGBDataSink.Listener {
+	private BufferedImage getBufferedImage(int width, int height) {
+		if (currentImage != null && currentImage.getWidth() == width
+				&& currentImage.getHeight() == height) {
+			return currentImage;
+		}
+		if (currentImage != null) {
+			currentImage.flush();
+		}
+		currentImage = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_RGB);
+		currentImage.setAccelerationPriority(0.0f);
+		return currentImage;
+	}
 
-        public void rgbFrame(int width, int height, IntBuffer rgb) {
-            if (!bufferLock.tryLock()) {
-                return;
-            }
-            if (updatePending) {
-                bufferLock.unlock();
-                return;
-            }
-            try {
-                final BufferedImage renderImage = getBufferedImage(width,
-                        height);
-                int[] pixels = ((DataBufferInt) renderImage.getRaster().getDataBuffer()).getData();
-                rgb.get(pixels, 0, width * height);
-                updatePending = true;
-            } finally {
-                bufferLock.unlock();
-            }
-            // Tell Canvas to use the new buffer
-            if (!isDisposed()) {
-                getDisplay().asyncExec(update);
-            }
-        }
-    }
+	public void setBackGroundColor(Color bgColor) {
+		this.bgColor = bgColor;
+	}
+
+	public Color getBackGroundColorColor() {
+		return bgColor;
+	}
+
+	private class RGBListener implements RGBDataSink.Listener {
+
+		public void rgbFrame(int width, int height, IntBuffer rgb) {
+			if (!bufferLock.tryLock()) {
+				return;
+			}
+			if (updatePending) {
+				bufferLock.unlock();
+				return;
+			}
+			try {
+				final BufferedImage renderImage = getBufferedImage(width,
+						height);
+				int[] pixels = ((DataBufferInt)renderImage.getRaster().getDataBuffer()).getData();
+				rgb.get(pixels, 0, width * height);
+				updatePending = true;
+			} finally {
+				bufferLock.unlock();
+			}
+			// Tell Canvas to use the new buffer
+			if (!isDisposed()) {
+				getDisplay().asyncExec(update);
+			}
+		}
+	}
 }
