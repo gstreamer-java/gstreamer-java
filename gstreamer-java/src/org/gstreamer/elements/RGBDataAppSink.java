e18bb6837968f1f9869374568e56a8c916840a9f
andres.colubri <andres.colubri@3c146ecf-7c30-0410-aadf-9b72ef142954>
2009-04-18 05:11:39 +0000

    Added RGBDataAppSink class Added new constructor to RGBDataSink that takes a pipeline as a parameter. The pipeline is used to extract the FakeSink element.

--- gstreamer-java/src/org/gstreamer/elements/RGBDataAppSink.java
+++ gstreamer-java/src/org/gstreamer/elements/RGBDataAppSink.java
@@ -1,6 +1,6 @@
-/* 
- * Copyright (c) 2007 Wayne Meissner
- * 
+/*
+ * Copyright (c) 2009 Andres Colubri
+ *
  * This file is part of gstreamer-java.
  *
  * This code is free software: you can redistribute it and/or modify it under
@@ -18,43 +18,37 @@
 
 package org.gstreamer.elements;
 
+import com.sun.jna.Pointer;
 import java.nio.IntBuffer;
-
 import org.gstreamer.Bin;
 import org.gstreamer.Buffer;
 import org.gstreamer.Caps;
 import org.gstreamer.Element;
 import org.gstreamer.ElementFactory;
 import org.gstreamer.GhostPad;
-import org.gstreamer.Pad;
 import org.gstreamer.Pipeline;
 import org.gstreamer.Structure;
 import org.gstreamer.lowlevel.GstBinAPI;
 import org.gstreamer.lowlevel.GstNative;
 
-
-public class RGBDataSink extends Bin {
+public class RGBDataAppSink extends Bin {
     private static final GstBinAPI gst = GstNative.load(GstBinAPI.class);
     private boolean passDirectBuffer = false;
     private final Listener listener;
-    private final BaseSink videosink;
-    
+    private final AppSink sink;
+
     public static interface Listener {
         void rgbFrame(int width, int height, IntBuffer rgb);
     }
-    
-    /**
-     * Creates a new instance of RGBDataSink with the given name.
-     * 
-     * @param name The name used to identify this pipeline.
-     */
-    public RGBDataSink(String name, Listener listener) {
+
+    public RGBDataAppSink(String name, Listener listener) {
         super(initializer(gst.ptr_gst_bin_new(name)));
         this.listener = listener;
-        videosink = (BaseSink) ElementFactory.make("fakesink", "VideoSink");
-        videosink.set("signal-handoffs", true);
-        videosink.set("sync", true);
-        videosink.connect(new VideoHandoffListener());
+
+        sink = (AppSink) ElementFactory.make("appsink", "VideoSink");
+        sink.set("emit-signals", true);
+        sink.set("sync", true);
+        sink.connect(new AppSinkNewBufferListener());
         
         //
         // Convert the input into 32bit RGB so it can be fed directly to a BufferedImage
@@ -62,50 +56,59 @@ public class RGBDataSink extends Bin {
         Element conv = ElementFactory.make("ffmpegcolorspace", "ColorConverter");
         Element videofilter = ElementFactory.make("capsfilter", "ColorFilter");
         videofilter.setCaps(new Caps("video/x-raw-rgb, bpp=32, depth=24"));
-        addMany(conv, videofilter, videosink);
-        Element.linkMany(conv, videofilter, videosink);
-        
+        addMany(conv, videofilter, sink);
+        Element.linkMany(conv, videofilter, sink);
+
         //
         // Link the ghost pads on the bin to the sink pad on the convertor
         //
         addPad(new GhostPad("sink", conv.getStaticPad("sink")));
     }
 
-    public RGBDataSink(String name, Pipeline pipeline, Listener listener) {
+    public RGBDataAppSink(String name, Pipeline pipeline, Listener listener) {
         super(initializer(gst.ptr_gst_bin_new(name)));
         this.listener = listener;
 
-        videosink = (BaseSink) pipeline.getElementByName("VideoSink");
-        videosink.set("signal-handoffs", true);
-        videosink.set("sync", true);
-        videosink.connect(new VideoHandoffListener());
+        // Doesn't work. getElementByName returns a BaseSink which cannot be casted
+        // to AppSink.
+        sink = (AppSink) pipeline.getElementByName("VideoSink");
+        sink.set("emit-signals", true);
+        sink.set("sync", true);
+        sink.connect(new AppSinkNewBufferListener());
     }
 
     /**
-     * Indicate whether the {@link RGBDataSink} should pass the native {@link java.nio.IntBuffer}
+     * Indicate whether the {@link RGBDataAppSink} should pass the native {@link java.nio.IntBuffer}
      * to the listener, or should copy it to a heap buffer.  The default is to pass
      * a heap {@link java.nio.IntBuffer} copy of the data
-     * @param passThru If true, pass through the native IntBuffer instead of 
+     * @param passThru If true, pass through the native IntBuffer instead of
      * copying it to a heap IntBuffer.
      */
     public void setPassDirectBuffer(boolean passThru) {
         this.passDirectBuffer = passThru;
     }
-    
+
     /**
      * Gets the actual gstreamer sink element.
-     * 
-     * @return a BaseSink
+     *
+     * @return a AppSink
      */
     public BaseSink getSinkElement() {
-        return videosink;
+        return sink;
     }
-    
-    class VideoHandoffListener implements Element.HANDOFF {
-        public void handoff(Element element, Buffer buffer, Pad pad) {
+
+    /**
+     * A listener class that handles the new-buffer signal from the AppSink element.
+     *
+     */
+    class AppSinkNewBufferListener implements AppSink.NEW_BUFFER {
+        public void newBuffer(Element elem, Pointer userData)
+        {
+            Buffer buffer = sink.pullBuffer();
+
             Caps caps = buffer.getCaps();
             Structure struct = caps.getStructure(0);
-            
+
             int width = struct.getInteger("width");
             int height = struct.getInteger("height");
             if (width < 1 || height < 1) {
@@ -119,9 +122,9 @@ public class RGBDataSink extends Bin {
                 rgb.put(buffer.getByteBuffer().asIntBuffer()).flip();
             }
             listener.rgbFrame(width, height, rgb);
-            
+
             //
-            // Dispose of the gstreamer buffer immediately to avoid more being 
+            // Dispose of the gstreamer buffer immediately to avoid more being
             // allocated before the java GC kicks in
             //
             buffer.dispose();
