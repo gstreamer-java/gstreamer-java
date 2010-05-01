3de21fa5302ed66ffc5da664aa4e503dc9ca7689
andres.colubri <andres.colubri@3c146ecf-7c30-0410-aadf-9b72ef142954>
2010-05-01 10:51:35 +0000

    Fixed issue 26: g_object_get doesnt return valid pointer

--- gstreamer-java/src/org/gstreamer/elements/DecodeBin2.java
+++ gstreamer-java/src/org/gstreamer/elements/DecodeBin2.java
@@ -18,6 +18,12 @@
 
 package org.gstreamer.elements;
 
+import org.gstreamer.Bin;
+import org.gstreamer.Element;
+import org.gstreamer.Pad;
+import org.gstreamer.lowlevel.GstAPI.GstCallback;
+import com.sun.jna.Pointer;
+
 /**
  * Utility {@link Element} to automatically identify media stream types and hook
  * up elements.
@@ -34,5 +40,43 @@ public class DecodeBin2 extends DecodeBin {
     
     public DecodeBin2(Initializer init) {
         super(init);
-    }   
+    }
+    
+
+    /**
+     * Signal emitted when this {@link DecodeBin} decodes a new pad.
+     */
+    public static interface NEW_DECODED_PAD {
+        /**
+         * 
+         * @param element The element which has the new Pad.
+         * @param pad the new Pad.
+         * @param last (unknown)
+         */
+        public void newDecodedPad(Element element, Pad pad, boolean last);
+    }
+    
+    /**
+     * Adds a listener for the <code>new-decoded-pad</code> signal
+     * 
+     * @param listener Listener to be called when a new {@link Pad} is encountered
+     * on the {@link Element}
+     */
+    public void connect(final NEW_DECODED_PAD listener) {
+        connect("new-decoded-pad", NEW_DECODED_PAD.class, listener, new GstCallback() {
+            @SuppressWarnings("unused")
+            public void callback(Element elem, Pad pad, boolean last, Pointer user_data) {
+                listener.newDecodedPad(elem, pad, last);
+            }
+        });
+    }
+    
+    /**
+     * Removes a listener for the <code>new-decoded-pad</code> signal
+     * 
+     * @param listener The listener that was previously added.
+     */
+    public void disconnect(NEW_DECODED_PAD listener) {
+        disconnect(NEW_DECODED_PAD.class, listener);
+    }    
 }
