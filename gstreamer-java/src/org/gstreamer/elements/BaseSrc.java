e0b14b112c7ba447cf0e158805814b44ca034982
lfarkas <lfarkas@3c146ecf-7c30-0410-aadf-9b72ef142954>
2010-09-24 18:19:56 +0000

    add all basesrc and basesink function

--- gstreamer-java/src/org/gstreamer/elements/BaseSrc.java
+++ gstreamer-java/src/org/gstreamer/elements/BaseSrc.java
@@ -26,38 +26,59 @@ import org.gstreamer.lowlevel.BaseAPI;
 import org.gstreamer.lowlevel.GstNative;
 
 public class BaseSrc extends Element {
-
-    private static interface BaseSrcAPI extends BaseAPI {        
+	public static interface BaseSrcAPI extends BaseAPI {
         FlowReturn gst_base_src_wait_playing(BaseSrc src);
 
         void gst_base_src_set_live(BaseSrc src, boolean live);
-
         boolean gst_base_src_is_live(BaseSrc src);
 
         void gst_base_src_set_format(BaseSrc src, Format format);
 
-        boolean gst_base_src_query_latency(BaseSrc src, boolean[] live,
-                ClockTime[] min_latency,
-                ClockTime[] max_latency);
+        boolean gst_base_src_query_latency(BaseSrc src, boolean[] live, ClockTime[] min_latency, ClockTime[] max_latency);
 
-        void gst_base_src_set_do_timestamp(BaseSrc src, boolean live);
+        void gst_base_src_set_blocksize(BaseSrc src, long blocksize);
+        long gst_base_src_get_blocksize(BaseSrc src);
 
+        void gst_base_src_set_do_timestamp(BaseSrc src, boolean timestamp);
         boolean gst_base_src_get_do_timestamp(BaseSrc src);
+
+        boolean gst_base_src_new_seamless_segment(BaseSrc src, long start, long stop, long position);
     }
 
-    private static final BaseSrcAPI gst = GstNative.load("gstbase", BaseSrcAPI.class);
+    public static final BaseSrcAPI BASESRC_API = GstNative.load("gstbase", BaseSrcAPI.class);
 
     public BaseSrc(Initializer init) {
         super(init);
     }
 
-    public void setFormat(Format format) {
-    	gst.gst_base_src_set_format(this, format);
+    public FlowReturn waitPlaying() {
+    	return BASESRC_API.gst_base_src_wait_playing(this);
     }
     public void setLive(boolean live) {
-    	gst.gst_base_src_set_live(this, live);
+    	BASESRC_API.gst_base_src_set_live(this, live);
     }
     public boolean isLive() {
-        return gst.gst_base_src_is_live(this);
+        return BASESRC_API.gst_base_src_is_live(this);
+    }
+    public void setFormat(Format format) {
+    	BASESRC_API.gst_base_src_set_format(this, format);
+    }
+    public boolean queryLatency(boolean[] live, ClockTime[] min_latency, ClockTime[] max_latency) {
+    	return BASESRC_API.gst_base_src_query_latency(this, live, min_latency, max_latency);
+    }
+    public void setBlocksize(long blocksize) {
+    	BASESRC_API.gst_base_src_set_blocksize(this, blocksize);
+    }
+    public long getBlocksize() {
+    	return BASESRC_API.gst_base_src_get_blocksize(this);
+    }
+    public void setTimestamp(boolean timestamp) {
+    	BASESRC_API.gst_base_src_set_do_timestamp(this, timestamp);
+    }
+    public boolean getTimestamp() {
+    	return BASESRC_API.gst_base_src_get_do_timestamp(this);
+    }
+    public boolean newSeamlessSegment(long start, long stop, long position) {
+    	return BASESRC_API.gst_base_src_new_seamless_segment(this, start, stop, position);
     }
 }
