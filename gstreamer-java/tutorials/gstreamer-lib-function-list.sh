936d4460a80c6ea25cfa4bdfdef970e5c57db7d3
lfarkas <lfarkas@3c146ecf-7c30-0410-aadf-9b72ef142954>
2011-03-30 21:59:08 +0000

    update function list

--- gstreamer-java/tutorials/gstreamer-lib-function-list.sh
+++ gstreamer-java/tutorials/gstreamer-lib-function-list.sh
@@ -2,7 +2,7 @@
 # regenerate gstreamer-lib-function-list.txt
 
 cd /usr/lib
-for i in `find . -maxdepth 1 -name 'libgst*' -a -type f`; do
+for i in `find . -maxdepth 1 -name 'libgst*' -a -type f|sort`; do
 	echo "================= $i ================="
 	nm -D -C $i|grep T|cut -f3 -d' '
 done
