d5ca99d9ebe2f4811bffbd37d56c2a6a3ea288ac
lfarkas@lfarkas.org <lfarkas@lfarkas.org@3c146ecf-7c30-0410-aadf-9b72ef142954>
2011-06-23 23:08:44 +0000

    fix compile warning

--- gstreamer-java/src/org/gstreamer/lowlevel/GNative.java
+++ gstreamer-java/src/org/gstreamer/lowlevel/GNative.java
@@ -173,6 +173,7 @@ public final class GNative {
             this.proxy = Proxy.getInvocationHandler(library);
         }
         
+        @SuppressWarnings("null")
         public Object invoke(Object self, Method method, Object[] args) throws Throwable {
             int lastArg = args != null ? args.length : 0;
             if (method.isVarArgs()) {
