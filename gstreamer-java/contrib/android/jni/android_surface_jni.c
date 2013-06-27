da4ab4e2c376888c26b9748ec70351fbcf61088b
tshalif <tshalif@3c146ecf-7c30-0410-aadf-9b72ef142954>
2013-06-27 12:21:38 +0000

    remove unused code in contrib/android/jni/android_surface_jni.c and set output SO name to libandroid_jgst.so

--- gstreamer-java/contrib/android/jni/android_surface_jni.c
+++ gstreamer-java/contrib/android/jni/android_surface_jni.c
@@ -1,30 +1,16 @@
-#include <string.h>
 #include <jni.h>
-#include <android/log.h>
 #include <android/native_window.h>
 #include <android/native_window_jni.h>
-#include <gst/gst.h>
 
-GST_DEBUG_CATEGORY_STATIC (debug_category);
-#define GST_CAT_DEFAULT debug_category
-
-/*
- * Private methods
- */
-
-static jlong get_native_surface_window (JNIEnv *env, jobject thiz, jobject surface) {
-
-  return ANativeWindow_fromSurface(env, surface);
-
-}
-
-
-/*
- * Private methods
- */
+// following prevents different size [-Wpointer-to-int-cast] warning
+#if GLIB_SIZEOF_VOID_P == 8
+# define POINTER_TO_JLONG_CAST (jlong)
+#else
+# define POINTER_TO_JLONG_CAST (jlong)(jint)
+#endif
 
 jlong Java_org_gstreamer_android_GstAndroidSurfaceAttach_nativeSurfaceWindow (JNIEnv *env, jobject thiz, jobject surface) {
 
-  return get_native_surface_window(env, thiz, surface);
+  return POINTER_TO_JLONG_CAST ANativeWindow_fromSurface(env, surface);
 
 }
